9bbf16726d1ab9a742f5ad99e44ade11a03a92f2
Taylor Pantiga <tpantiga@bayareametro.gov>
2023-08-11 14:55:44 -0700

    Plot station points on map.
    Adjusted the latitude and longitutde bounds of the basemap. Added the site coordinates as a dataframe and plotted the coordinates on the map.

--- CalCOFI_map.R
+++ CalCOFI_map.R
@@ -2,6 +2,18 @@ library(ggplot2)
 library(ggmap)
 
 #set basemap
-basemap <- get_stamenmap(bbox =c(left = -123.737691, bottom = 32.000, right = -117.713201, top = 39.507131), maptype = c("terrain-background"))
+basemap <- get_stamenmap(bbox =c(left = -126.000, bottom = 29.000, right = -117.000, top = 39.500), maptype = c("terrain-background"))
 ggmap(basemap)
+
+#load data
+sites <- read.csv("Data/CalCOFI_stations_116.csv")
+
+#add station points
+ggmap(basemap) + xlab("longitude") + ylab("latitutde") +
+  geom_point(data=sites, aes(x=Dlongitude, y=Dlatitude), 
+             shape = 16, color="black", size=1)
+
+
+
+
 

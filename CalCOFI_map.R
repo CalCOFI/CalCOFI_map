1d93fd9cfb3a47ba61dd80032ab1d640e7e8c633
Ben Best <ben@ecoquants.com>
2023-09-21 14:33:42 -0700

    + BOEM wind areas

--- CalCOFI_map.R
+++ CalCOFI_map.R
@@ -1,17 +1,55 @@
-library(ggplot2)
-library(ggmap)
+if (!"librarian" %in% installed.packages())
+  install.packages("librarian")
+
+librarian::shelf(
+  CalCOFI/calcofi4r,
+  dplyr, ggmap, ggplot2, mapview, sf)
+
+# get BOEM wind areas
+plys_boem <- cc_places |> 
+  filter(category == "BOEM Wind Planning Areas")
+st_geometry(plys_boem) = "geometry" # for geom_sf()
+
+# load stations
+pts_stations <- read.csv("Data/calcofi_sta_master_v.1.2.csv") |> 
+  filter(
+    !is.na(lon), 
+    !is.na(lat),
+    lon < -115) |> # remove orderocc==104 in Yellow Sea?!
+  st_as_sf(
+    coords = c("lon", "lat"),
+    remove = F,
+    crs = 4326)
+# mapView(pts_stations)
+
+bb <- st_bbox(pts_stations)
 
 #set basemap
-basemap <- get_stamenmap(bbox =c(left = -126.000, bottom = 29.000, right = -117.000, top = 39.500), maptype = c("terrain-background"))
+basemap <- get_stamenmap(
+  bbox    = c(
+    left   = bb[['xmin']], 
+    bottom = bb[['ymin']], 
+    right  = bb[['xmax']], 
+    top    = bb[['ymax']]),
+  zoom = 7,
+  maptype = c("terrain-background"))
 ggmap(basemap)
 
-#load data
-sites <- read.csv("Data/calcofi_sta_master_v.1.2.csv")
-
 #add station points
-ggmap(basemap) + xlab("longitude") + ylab("latitutde") +
-  geom_point(data=sites, aes(x=lon, y=lat, color=leg), 
-             shape = 16, size=1.25)+
-  scale_color_manual(values=c("core" = "forestgreen", "north" = "darkred"))
-ggsave("draft_CalCOFI_map_9-20-23.jpg")
+ggmap(basemap) + 
+  xlab("longitude") + 
+  ylab("latitude") +
+  geom_sf(
+    data = plys_boem,
+    inherit.aes = F,
+    color = "darkblue", fill="blue") +
+  geom_sf(
+    data = pts_stations,
+    inherit.aes = F,
+    shape = 16, size=1.25,
+    mapping = aes(color = leg)) +
+  scale_color_manual(
+    values = c("core" = "forestgreen", "north" = "darkred"))
+ggsave("draft_CalCOFI_map_boem.jpg")
+
 

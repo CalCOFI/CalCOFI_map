a63c29d9f6ce8b726df172ebe88b696f6e5bc174
Taylor Pantiga <tpantiga@bayareametro.gov>
2023-09-22 14:53:11 -0700

    Change base map, add BOEM wind areas, adjust style add new base map new code for adding the site coordinates add BOEM wind areas stlye adjustments to map (colors, labels, legend)

--- CalCOFI_map.R
+++ CalCOFI_map.R
@@ -1,17 +1,45 @@
 library(ggplot2)
 library(ggmap)
+library(dplyr)
+library(sf)
+library(rnaturalearth)
+library(rnaturalearthdata)
 
 #set basemap
-basemap <- get_stamenmap(bbox =c(left = -126.000, bottom = 29.000, right = -117.000, top = 39.500), maptype = c("terrain-background"))
-ggmap(basemap)
+world <- ne_countries(scale = "medium", returnclass = "sf")
 
-#load data
-sites <- read.csv("Data/calcofi_sta_master_v.1.2.csv")
+#load station data
+sites <- read.csv("Data/calcofi_sta_master_v.1.2.csv") |>
+  st_as_sf(
+    coords = c("lon", "lat"),
+    remove = F,
+    crs = 4326)
 
-#add station points
-ggmap(basemap) + xlab("longitude") + ylab("latitutde") +
-  geom_point(data=sites, aes(x=lon, y=lat, color=leg), 
-             shape = 16, size=1.25)+
-  scale_color_manual(values=c("core" = "forestgreen", "north" = "darkred"))
-ggsave("draft_CalCOFI_map_9-20-23.jpg")
+#get BOEM wind areas
+boem <- cc_places |>
+  filter(category == "BOEM Wind Planning Areas")
+st_geometry(boem) = "geometry"
+
+#add station points to map
+ggplot(data = world) +
+  xlab("longitute") +
+  ylab("latitutde")+
+  geom_sf()+
+  geom_point(data = sites, aes(x = lon, y = lat, color = Station), size = 0.75,
+             shape = 16)+
+  scale_color_manual(values=c("Core stations" = "#1F40C7", 
+                              "North stations" = "#ECCE15", 
+                              "Pilot stations" = "#7B7777" ))+
+  geom_sf(data = boem, inherit.aes = F,
+          color = "darkred", fill = NA) +
+  coord_sf(xlim = c(-127, -117), ylim = c(29, 39.5), expand = FALSE)+
+  theme_classic()+
+  theme(legend.title=element_blank())
+
+#save image
+ggsave("draft_CalCOFI_map_9-22-23_sm_pt.jpg")
+
+
+
+
 

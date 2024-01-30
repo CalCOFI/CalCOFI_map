6b7cb9e334887568d63334325edb2a935bbbc9a5
Taylor Pantiga <tpantiga@bayareametro.gov>
2024-01-30 10:27:41 -0800

    Organization edits

--- CalCOFI_map.R
+++ CalCOFI_map.R
@@ -8,30 +8,46 @@ library(remotes)
 library(calcofi4r)
 
 
-#set basemap
+#set basemap----
 world <- ne_countries(scale = "medium", returnclass = "sf")
 
-#load station data
+#load station data----
 sites <- read.csv("Data/calcofi_sta_master_v.1.2.csv") |>
   st_as_sf(
     coords = c("lon", "lat"),
     remove = F,
     crs = 4326)
 
-#get BOEM wind areas
+#Interest area layers----
+##BOEM wind areas----
 boem <- cc_places |>
   filter(category == "BOEM Wind Planning Areas")
 st_geometry(boem) = "geometry"
 
-#National Marine Sanctuaries area
+##National Marine Sanctuaries area----
 nms <- cc_places |>
   filter(category == "National Marine Sanctuaries")
 st_geometry(nms)="geometry"
 
-#NOAA aquaculture opportunity areas
+##NOAA aquaculture opportunity areas----
 aqua <- cc_places |>
   filter(category == "NOAA Aquaculture Opportunity Areas")
 st_geometry(aqua) =  "gemoetr"
+
+##Add MPA shapefile----
+unzip('Data/California_Marine_Protected_Areas_[ds582].zip', exdir = 'data')
+file.remove('Data/California_Marine_Protected_Areas_[ds582].zip')
+
+MPA <- read_sf('Data/California_Marine_Protected_Areas_[ds582].shp')
+MPA_4326 <- MPA %>% 
+  st_transform(crs=4326)
+
+##Add state waters (3 nautical miles) layer----
+unzip('state_waters.zip', exdir = 'data')
+
+st_water <- read_sf("Data/CA_cst3nm.shp")
+st_water_4326 <- st_water %>%
+  st_transform(crs = 4326)
 
 ##Areas of special biological significance----
 unzip('Data/Areas_of_Special_Biological_Significance.zip')
@@ -39,8 +55,8 @@ file.remove('Data/Areas_of_Special_Biological_Significance.zip')
 
 bio_sig <- read_sf('Data/Areas_of_Special_Biological_Significance.shp')
 
-
-#add station points to map + BOEM and NMS + state waters
+#Maps----
+##add station points to map + BOEM and NMS + state waters----
 ggplot(data = world) +
   xlab("longitute") +
   ylab("latitutde")+
@@ -58,7 +74,7 @@ ggplot(data = world) +
   coord_sf(xlim = c(-127, -117), ylim = c(29, 39.5), expand = FALSE)+
   theme_classic()
 
-#map with NOAA aquaculture opportunity areas
+##map with NOAA aquaculture opportunity areas----
 
 ggplot(data = world) +
   xlab("longitute") +
@@ -90,28 +106,8 @@ ggplot(data = world) +
   coord_sf(xlim = c(-127, -117), ylim = c(29, 39.5), expand = FALSE)+
   theme_classic()+
   theme(legend.title=element_blank())
- 
 
-#save image
-ggsave("draft_CalCOFI_map_9-27-23.jpg")
-
-
-#Add MPA shapefile
-unzip('Data/California_Marine_Protected_Areas_[ds582].zip', exdir = 'data')
-file.remove('Data/California_Marine_Protected_Areas_[ds582].zip')
-
-MPA <- read_sf('Data/California_Marine_Protected_Areas_[ds582].shp')
-MPA_4326 <- MPA %>% 
-  st_transform(crs=4326)
-
-#Add state waters (3 nautical miles) layer
-unzip('state_waters.zip', exdir = 'data')
-
-st_water <- read_sf("Data/CA_cst3nm.shp")
-st_water_4326 <- st_water %>%
-  st_transform(crs = 4326)
-
-#map with state waters and MPA
+##map with state waters and MPA----
 ggplot(data = world) +
   xlab("longitute") +
   ylab("latitutde")+
